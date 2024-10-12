c1488bd7fb083db22308ab6ff25032548e7413fb
Dmytro Shavronskyy <dima.shavronskyi@gmail.com>
2024-10-12 17:47:15 +0300

    created web(mvc) project, also consumed mvc with web api

--- VacanciesScrapper_Web/Models/VacancyViewModel.cs
+++ VacanciesScrapper_Web/Models/VacancyViewModel.cs
@@ -6,9 +6,11 @@
 
         public string Description { get; set; }
 
-        public string ShortDescription { get; set; }
+        public string shortDescription { get; set; }
 
         public string Company { get; set; }
+
+        public string CompanyImg { get; set; }
 
         public string Salary { get; set; }
 
